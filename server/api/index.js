0fe41b8800d6111cc42756586ec925d8ba873a36
SreenithiRamesh <sreenithiramesh2@gmail.com>
2025-04-18 23:50:46 +0530

    Your commit message here

---
+++ server/api/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import serverless from 'serverless-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from '../server/configs/mongodb.js';
+import { clerkWebhooks } from '../server/controllers/webhooks.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+await connectDB();
+
+// Routes
+app.get('/', (req, res) => res.send("API Working ✅"));
+app.post('/clerk', clerkWebhooks);
+
+export const handler = serverless(app);
